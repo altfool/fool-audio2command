7589e4ad820b46b3d169565b11d8115424d0f53d
Zongming Yang <zongmiy@clemson.edu>
2020-09-24 20:45:06 -0400

    initial version3

--- audio2command.py
+++ audio2command.py
@@ -1,6 +1,6 @@
 from global_params import *
 import keyword_commands
-import os
+import os, re
 
 ##======= extract keyword modules =============##
 keyword_module_file_postfix = ".py"
@@ -9,17 +9,19 @@ keyword_module_dir_name = "keyword_commands"
 keyword_module_dir_path = os.path.join(os.path.dirname(__file__), keyword_module_dir_name)
 keyword_module_candidate = os.listdir(keyword_module_dir_path)
 keyword_module_set = {kw[:-3] for kw in keyword_module_candidate if kw[-3:] == keyword_module_file_postfix and kw[:2] != "__" }
-print(keyword_module_set)
+print("keyword set: {}\n".format(keyword_module_set))
 
 ##======== listen to audio ================##
 
 ##======== convert audio to text ===========##
 mytext = "click this please"
-int("your text msg: {}".format(mytext))
+print("your text msg: {}".format(mytext))
 
 ##======== convert text to commands ==========##
 ###### stem text ########
-mytext_list = mytext.split("\n\t ")
+mytext_list = re.split("\n|\t| ", mytext)
+mytext_stem = [item for item in mytext_list if item not in stopwords_list]
+print("stem text msg: {}".format(mytext_stem))
 
 
-keyword_commands.click.keyword_click()
+keyword_commands.keyword_click()
--- global_params.py
+++ global_params.py
@@ -1,2 +1,16 @@
+import os
 
+color_green = '\x1b[5;30;42m'
+color_red = '\x1b[1;30;41m'
+color_end = '\x1b[0m'
 
+# stop words modified from https://countwordsfree.com/stopwords
+stopwords_filename = "stopwords.txt"
+stopwords_filepath = os.path.join(os.path.dirname(__file__), stopwords_filename)
+# print(stopwords_filepath)
+with open(stopwords_filepath, 'r') as fd:
+    lines = fd.readlines()
+    stopwords_list = set([line.strip() for line in lines])
+    print("load stop words...\t" + color_green + " Done " + color_end)
+# print(stopwords_list)
+# print(len(stopwords_list))
